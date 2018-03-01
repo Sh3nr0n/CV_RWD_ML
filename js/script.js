8890b07b048caebfc32d49a1e799f0e965ce34bf
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-03-01 23:31:18 +0100

    header, footer, hobbies added, h1 tag added, navbar mooved to header

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
 $(document).ready(function () {
-
     $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 300
-        edge: 'left', // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true, // Choose whether you can drag to open on touch screens,
-        /*onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened*/
-        /*onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed*/
+        menuWidth: 300,
+        edge: 'left',
+        closeOnClick: true,
+        draggable: true, 
     });
-
     $('.parallax').parallax();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
-    
-    
-    //Use "scroll fire" plugin above to display content when the user scrolls to a fixed part of the page
-
-    // var options = [
-    //     {selector: '.class', offset: 200, callback: customCallbackFunc },
-    //     {selector: '.other-class', offset: 200, callback: function() {
-    //       customCallbackFunc();
-    //     } },
-    //   ];
-    //   Materialize.scrollFire(options);
 });
