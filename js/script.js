808432f1b11154570595f5e6eb3c0c3c8b6a9133
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-03-08 09:17:46 +0100

    click photo + liste competences

--- js/script.js
+++ js/script.js
@@ -8,4 +8,29 @@ $(document).ready(function () {
     $('.parallax').parallax();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
     $('.modal').modal();
+    var i = 0;
+    var sound;
+    $("#photo").on("click", function(){
+        ++i;
+        switch(i){
+            case 1:
+            console.log("aie1");
+            //sound = "sound/fichierAïe.mp3";
+            break;
+            case 2:
+            console.log("aie2");
+            sound = "sound/fichierOuïe.mp3";
+            break;
+            case 3:
+            console.log("aie3");
+            //sound = "sound/fichierCestFiniOui.mp3";
+            i=0;
+            break;
+            default:
+            console.log("default");
+            //sound = "sound/fichierAïe.mp3";
+            break;
+        }
+
+    });
 });
