9cd95f864e47bb500075b6f16c483e4429568b9a
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-03-11 16:48:57 +0100

    php form to test online

--- js/script.js
+++ js/script.js
@@ -1,36 +1,81 @@
 $(document).ready(function () {
+
     $('.button-collapse').sideNav({
         menuWidth: 300,
         edge: 'left',
         closeOnClick: true,
         draggable: true,
     });
+
     $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+
     $('.modal').modal();
+
+    // Plays sounds when user clicks on my picture (portrait)
+
     var i = 0;
     var sound;
-    $("#photo").on("click", function(){
+    $("#photo").on("click", function () {
         ++i;
-        switch(i){
+        switch (i) {
             case 1:
-            console.log("aie1");
-            //sound = "sound/fichierAïe.mp3";
-            break;
+                console.log("aie1");
+                //sound = "sound/fichierAïe.mp3";
+                break;
             case 2:
-            console.log("aie2");
-            sound = "sound/fichierOuïe.mp3";
-            break;
+                console.log("aie2");
+                sound = "sound/fichierOuïe.mp3";
+                break;
             case 3:
-            console.log("aie3");
-            //sound = "sound/fichierCestFiniOui.mp3";
-            i=0;
-            break;
+                console.log("aie3");
+                //sound = "sound/fichierCestFiniOui.mp3";
+                i = 0;
+                break;
             default:
-            console.log("default");
-            //sound = "sound/fichierAïe.mp3";
-            break;
+                console.log("default");
+                //sound = "sound/fichierAïe.mp3";
+                break;
         }
 
     });
+
+    //Display a message when data are successfully sent
+
+    // $("#submitModal").on("click", function () {
+    //     // console.log("clicksubmit");
+    //     var $toastContent = $('<span>Votre message a bien été envoyé</span>');
+    //     Materialize.toast($toastContent, 5000);
+    // });
+
+    // Submit message using ajax
+
+    $("#submitModal").on("click", function () {
+        // Get the form.
+        var form = $('#contactForm');
+
+            // Submit the form using AJAX.
+            $.ajax({
+                type: 'POST',
+                url: "php/mailer.php",
+                data: $(form).serialize(),// Serialize the form data
+                success: function(){
+                    var $toastContent = $("<span>Votre message a bien été envoyé</span>");//.text(response);
+                    Materialize.toast($toastContent, 5000);
+                },
+
+                error: function () {
+                    var $toastContent = $("<span>Une erreur est survenue lors de l\'envoi de votre message. Veillez à remplir tous les champs.</span>");//.text(response);
+                    Materialize.toast($toastContent, 5000); 
+
+                }
+
+                
+            });
+
+    });
+
 });
---
+++ php/mailer.php
@@ -0,0 +1,56 @@
+<?php
+// $to = "mehdi.lefeuvre@gmail.com";
+// $subject = "test";
+// $message = "salut";
+// mail( $to , $subject , $message);
+// echo"stralalafifi";
+
+    // Only process POST requests
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
+
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Il semble y avoir eu un problème avec votre envoi. Veuillez renseigner tous les champs et essayer à nouveau.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        $recipient = "mehdi.lefeuvre@gmail.com";
+
+        // Set the email subject.
+        $subject = "Nouvelle demande de $name depuis la page du projet 'CV_RWD_ML'";
+
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Merci, votre message a bien été envoyé.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Une erreur a été rencontrée et votre message n'a pas pu être envoyé.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "Il semble y avoir eu un problème avec votre envoi, merci d'essayer à nouveau.";
+    }
+
+?>
